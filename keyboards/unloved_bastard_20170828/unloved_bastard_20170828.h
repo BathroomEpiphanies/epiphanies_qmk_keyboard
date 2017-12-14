9eca94310acc67be1326b8e22adfa344e423be39
Fredrik Atmer <epiphanies@atmer.se>
2017-12-14 10:06:36 +0100

    Added the Unloved Bastard
    Obviously no-one could possibly have used
    these since they were all broken. All
    should have been fixed now.
    
    Tested (by me)
    Black Petal 20131001
    Frosty Flake 20130602
    Frosty Flake 20140521
    Kitten Paw 20130602
    Kitten Paw 20160418
    Pegasus Hoof 20131001
    Pegasus Hoof 20150108
    
    Untested (but probable to work)
    Ghost Squid 20140518
    Unloved Bastard 20170828

--- keyboards/unloved_bastard_20170828/unloved_bastard_20170828.h
+++ keyboards/unloved_bastard_20170828/unloved_bastard_20170828.h
@@ -15,8 +15,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef FROSTY_FLAKE_20130602_H
-#define FROSTY_FLAKE_20130602_H
+#ifndef UNLOVED_BASTARD_20170828_H
+#define UNLOVED_BASTARD_20170828_H
 
 #include "matrix.h"
 #include "quantum.h"
@@ -29,12 +29,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	KJ2, KJ5, KI5, KH5, KG5, KG2, KF2, KF5, KE5, KD5, KR5, KR2, KE2, KB3,   KK4, KO7, KQ7, \
 	KI2, KJ3, KI3, KH3, KG3, KG6, KF6, KF3, KE3, KD3, KR3, KR6,      KB1,                  \
 	KN2, KI6, KJ1, KI1, KH1, KG1, KG0, KF0, KF1, KE1, KD1, KR0,      KN3,        KO6,      \
-	KA4, KP2, KC6,                KK6,                KC0, KM3, KD0, KA1,   KO0, KK0, KL0  \
+	KA4, KC2, KC6,                KK6,                KC0, KC3, KD0, KA1,   KO0, KK0, KL0  \
 	) { /*        00-A  01-B  02-C  03-D  04-E  05-F  06-G  07-H  08-I  09-J  10-K  11-L  12-M  13-N  14-O  15-P  16-Q  17-R */ \
 		/* 0 */  { ___ , KB0 , KC0 , KD0 , ___ , KF0 , KG0 , ___ , ___ , ___ , KK0 , KL0 , ___ , ___ , KO0 , ___ , ___ , KR0 }, \
 		/* 1 */  { KA1 , KB1 , ___ , KD1 , KE1 , KF1 , KG1 , KH1 , KI1 , KJ1 , ___ , ___ , ___ , ___ , ___ , ___ , ___ , ___ }, \
-		/* 2 */  { ___ , KB2 , ___ , KD2 , KE2 , KF2 , KG2 , KH2 , KI2 , KJ2 , ___ , ___ , ___ , KN2 , ___ , KP2 , ___ , KR2 }, \
-		/* 3 */  { ___ , KB3 , ___ , KD3 , KE3 , KF3 , KG3 , KH3 , KI3 , KJ3 , ___ , ___ , KM3 , KN3 , ___ , ___ , ___ , KR3 }, \
+		/* 2 */  { ___ , KB2 , KC2 , KD2 , KE2 , KF2 , KG2 , KH2 , KI2 , KJ2 , ___ , ___ , ___ , KN2 , ___ , ___ , ___ , KR2 }, \
+		/* 3 */  { ___ , KB3 , KC3 , KD3 , KE3 , KF3 , KG3 , KH3 , KI3 , KJ3 , ___ , ___ , ___ , KN3 , ___ , ___ , ___ , KR3 }, \
 		/* 4 */  { KA4 , KB4 , ___ , KD4 , KE4 , KF4 , KG4 , KH4 , KI4 , KJ4 , KK4 , KL4 , ___ , ___ , KO4 , ___ , KQ4 , KR4 }, \
 		/* 5 */  { KA5 , ___ , KC5 , KD5 , KE5 , KF5 , KG5 , KH5 , KI5 , KJ5 , ___ , ___ , ___ , ___ , ___ , ___ , ___ , KR5 }, \
 		/* 6 */  { ___ , KB6 , KC6 , ___ , KE6 , KF6 , KG6 , KH6 , KI6 , KJ6 , KK6 , ___ , ___ , ___ , KO6 , ___ , ___ , KR6 }, \
@@ -42,11 +42,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	}
 
 
-inline void caps_led_on(void)  { DDRB |=  (1<<7); PORTB &= ~(1<<7); }
-inline void caps_led_off(void) { DDRB &= ~(1<<7); PORTB &= ~(1<<7); }
+inline void caps_led_on(void)  { DDRC |=  (1<<6); PORTC &= ~(1<<6); }
+inline void caps_led_off(void) { DDRC &= ~(1<<6); PORTC &= ~(1<<6); }
 
-inline void scroll_led_on(void)  { DDRC |=  (1<<6); PORTC &= ~(1<<6); }
-inline void scroll_led_off(void) { DDRC &= ~(1<<6); PORTC &= ~(1<<6); }
+inline void scroll_led_on(void)  { DDRB |=  (1<<7); PORTB &= ~(1<<7); }
+inline void scroll_led_off(void) { DDRB &= ~(1<<7); PORTB &= ~(1<<7); }
 
 inline void fn_led_on(void)  { DDRC |=  (1<<5); PORTC &= ~(1<<5); }
 inline void fn_led_off(void) { DDRC &= ~(1<<5); PORTC &= ~(1<<5); }
